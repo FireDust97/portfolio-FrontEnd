419ec2446350e678378b15f10cd8884a9944df22
FireDust97 <lautarortiz472@gmail.com>
2023-04-20 21:00:41 -0300

    Se ha aplicado el método routing al proyecto. Se ha inicializado el prototipo del componente login.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HardSoftSkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
