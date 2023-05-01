427c2ad006db2c11a5414efc413d4256d30e1561
FireDust97 <lautarortiz472@gmail.com>
2023-05-01 19:42:56 -0300

    Se ha implementado Firebase al proyecto

--- src/app/service/experiencia-service.service.ts
+++ src/app/service/experiencia-service.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class ExperienciaServiceService {
-  expURL = 'http://localhost:8080/explab/'
+  expURL = 'https://backend-portfolioargprog.onrender.com/explab/'
 
   constructor(private httpClient: HttpClient) { }
 
