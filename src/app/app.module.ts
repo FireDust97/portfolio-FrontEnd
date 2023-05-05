2ae0799d9a4e152fe9866996d760c0fc9f439b47
FireDust97 <lautarortiz472@gmail.com>
2023-05-05 02:25:53 -0300

    Se ha agregado la funcionalidad para editar el perfil de acerca de

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { interceptorProvider } from './service/Interceptor-Service';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
 import { EditeducacionComponent } from './components/educacion/editeducacion.component';
+import { EditAcercaDeComponent } from './components/acerca-de/edit-acerca-de.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -35,7 +38,8 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
     NewExperienciaComponent,
     EditExperienciaComponent,
     NeweducacionComponent,
-    EditeducacionComponent
+    EditeducacionComponent,
+    EditAcercaDeComponent
   ],
   imports: [
     BrowserModule,
@@ -64,7 +68,9 @@ import { EditeducacionComponent } from './components/educacion/editeducacion.com
       "responsive": false,
       "startFromZero": false}),
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideStorage(() => getStorage())
   ],
   providers: [
   ],
